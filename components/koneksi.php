2fb7c4956256df2f0fb8bb974d45cc4a181dd1d5
Parhanlah <farhanadi18032002@gmail.com>
2026-06-23 11:36:51 +0700

    Siap deploy

--- components/koneksi.php
+++ components/koneksi.php
@@ -1,12 +1,21 @@
 <?php
 date_default_timezone_set('Asia/Jakarta'); 
-$host = "localhost";
-$user = "root"; // Default username XAMPP/Laragon
-$pass = "";     // Default password XAMPP/Laragon (kosong)
-$db   = "db_poncotani";
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+$host = "mysql-1bdbc819-farhanadi18032002-bdfb.h.aivencloud.com";
+$user = "avnadmin";
+$pass = getenv("DB_PASS"); // Coba ambil password dari variabel environment (Vercel)
+if (!$pass && file_exists(__DIR__ . '/password.php')) {
+    include __DIR__ . '/password.php'; // Ambil password dari file rahasia jika dijalankan di komputer lokal
+}
+$db   = "defaultdb"; 
+$port = 15608;
+
+// Menggunakan koneksi SSL wajib untuk Aiven
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+mysqli_real_connect($conn, $host, $user, $pass, $db, $port, NULL, MYSQLI_CLIENT_SSL);
 
 if (!$conn) {
     die("Koneksi Database Gagal: " . mysqli_connect_error());
 }
+?>
